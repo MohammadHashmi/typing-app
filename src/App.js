a628e38e5c575af973746ff4b90cb3beb2423807
Dubi-1 <60275618+MohammadHashmi@users.noreply.github.com>
2023-12-23 16:27:21 -0700

    Create react app and linked with firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBozoUvs-aWQ1IL3jj84mjeQuvpbMyE9ME",
+  authDomain: "chatthing-29689.firebaseapp.com",
+  projectId: "chatthing-29689",
+  storageBucket: "chatthing-29689.appspot.com",
+  messagingSenderId: "867317692166",
+  appId: "1:867317692166:web:e9359220a4ad4353969087",
+  measurementId: "G-YMWE7QPBNZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hey there</h1>
       </header>
     </div>
   );
